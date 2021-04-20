77e9dc1a32c2c0c377e75035a63a838816d329e0
emir91 <vranacemir@gmail.com>
2021-04-20 15:32:31 +0200

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+    entry: path.join(__dirname, './src/index.js'),
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'main.js'
+    },
+    externals: [nodeExternals()]
+}
